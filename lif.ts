a5a72fd33a51923fede746771f275974e4f6441a
Sean Aubin <seanaubin@gmail.com>
2017-04-13 14:56:13 +0900

    further preparations

---
+++ lif.ts
@@ -0,0 +1,82 @@
+export class SpikingLif {
+    alpha: number;
+    biasCurrent: number;
+    dt: number;
+    refac: boolean;
+    refacTime: number;
+    tRef: number;
+    tRc: number;
+    pot: number;
+
+    constructor(tRef: number, tRc: number, intercept: number, maxFire: number, radius: number) {
+        this.tRef = tRef;
+        this.tRc = tRc;
+
+        const beta = 1.0 / (
+            1.0 - Math.exp(
+                (-1.0/maxFire + tRef) / tRc
+            )
+        );
+
+        this.alpha = (1.0 - beta) / (intercept + radius*1.0);
+        this.biasCurrent = 1.0 - this.alpha * intercept;
+
+        this.refac = false;
+        this.refacTime = 0.0;
+        this.pot = 0.0;
+
+        this.dt = 0.001;
+    }
+
+    spikes(input: number) {
+        return this.calcSpike(this.current(input));
+    }
+
+    activity(input: number) {
+        const current = input * this.alpha + this.biasCurrent;
+
+        if(current > 1){
+			return 1.0 / (
+                this.tRef - this.tRc * Math.log(1-1/current)
+            );
+        } else {
+			return 0.0;
+        }
+    }
+
+    current(input: number) {
+        const current = input * this.alpha + this.biasCurrent;
+
+		if(current > 1){
+			return current;
+        } else {
+			return 0.0;
+        }
+
+    }
+
+    protected calcSpike(current: number) {
+        if (!this.refac) {
+
+            const dV = 1.0 / this.tRc * (current - this.pot) * this.dt;
+            this.pot += dV;
+
+            if (this.pot >= 1) {
+                this.refac = true;
+                this.pot = 0.0;
+                return 2.0;
+            } else {
+                return this.pot;
+            }
+        } else {
+            
+            this.refacTime += this.dt;
+            if(this.refacTime >= this.tRef) {
+                this.refac = false;
+                this.refacTime = 0.0;
+            }
+
+            return 0.0;
+        }
+    }
+}
--- princip1_and_2.ts
+++ princip1_and_2.ts
@@ -1,6 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
+import { SpikingLif } from "./lif"
+
+document.addEventListener("DOMContentLoaded", () => {
+    // First, make the basic example with few neurons
+
     // make an ensemble of neurons
-    // calculate their decoders
+    const neuronNumber = 4;
+
+    // load the intercepts
+    const intercepts: number[] = [];
+    // load the max firing rates
+    const maxFires: number[] = [];
+    const neurons: SpikingLif[] = [];
+    for (let nIdx = 0; nIdx < neuronNumber; nIdx++) {
+        neurons.push(
+            new SpikingLif(0.002, 0.02, intercepts[nIdx], maxFires[nIdx], 1.0)
+        );
+    }
+
+    // load their encoders
+    // load their decoders
+
+    // Then make the example with more neurons
 
     // make a basic event loop to act as a timer
     window.setInterval(() => {
@@ -16,100 +36,6 @@ document.addEventListener('DOMContentLoaded', () => {
         // show the output of two functions
     }
 });
-
-function calcDecoders(activities, target) {
-
-    // TODO: figure out how to do these math operations
-    const dx = activities.shape[0];
-    const gamma = np.dot(activities, activities.T) * dx
-	const upsilon = np.dot(activities, target) * dx
-	const decoders = np.dot(np.linalg.pinv(gamma), upsilon)
-    return decoders
-}
-
-// make a basic LIF neuron model
-class SpikingLif {
-    alpha: number;
-    biasCurrent: number;
-    dt: number;
-    refac: boolean;
-    refacTime: number;
-    tRef: number;
-    tRc: number;
-    pot: number;
-
-    constructor(tRef: number, tRc: number, intercept: number, maxFire: number, radius: number) {
-        this.tRef = tRef;
-        this.tRc = tRc;
-
-        const beta = 1.0 / (
-            1.0 - Math.exp(
-                (-1.0/maxFire + tRef) / tRc
-            )
-        );
-
-        this.alpha = (1.0 - beta) / (intercept + radius*1.0);
-        this.biasCurrent = 1.0 - this.alpha * intercept;
-
-        this.refac = false;
-        this.refacTime = 0.0;
-        this.pot = 0.0;
-
-        this.dt = 0.001;
-    }
-
-    spikes(input: number) {
-        return this.calcSpike(this.current(input));
-    }
-
-    activity(input: number) {
-        const current = input * this.alpha + this.biasCurrent;
-
-        if(current > 1){
-			return 1.0 / (
-                this.tRef - this.tRc * Math.log(1-1/current)
-            );
-        } else {
-			return 0.0;
-        }
-    }
-
-    current(input: number) {
-        const current = input * this.alpha + this.biasCurrent;
-
-		if(current > 1){
-			return current;
-        } else {
-			return 0.0;
-        }
-
-    }
-
-    protected calcSpike(current: number) {
-        if (!this.refac) {
-
-            const dV = 1.0 / this.tRc * (current - this.pot) * this.dt;
-            this.pot += dV;
-
-            if (this.pot >= 1) {
-                this.refac = true;
-                this.pot = 0.0;
-                return 2.0;
-            } else {
-                return this.pot;
-            }
-        } else {
-            
-            this.refacTime += this.dt;
-            if(this.refacTime >= this.tRef) {
-                this.refac = false;
-                this.refacTime = 0.0;
-            }
-
-            return 0.0;
-        }
-    }
-}
 
 // show the effect of adding more neurons
 
